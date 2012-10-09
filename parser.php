42fbd6d1d48209d1bf360ff73b94188801912ef1
Thijs Lensselink <thijs.lensselink@mobiletulip.com>
2012-10-09 09:49:15 +0200

    Updated parser to use Apache's logtail, Added license file

--- parser.php
+++ parser.php
@@ -1,31 +1,71 @@
 <?php
+/**
+ * SmppBox Log Parser (http://mobiletulip.com/)
+ *
+ * @link https://github.com/mobiletulip/php-logparser
+ * @copyright Copyright (c) 2012 Mobile Tulip (http://mobiletulip.com/)
+ * @license https://github.com/mobiletulip/php-logparser/license/new-bsd
+ * @package SmppBoxParser
+ */
+
+/**
+ * Class to parse Kannel SMPP logs and send stats to Graphite
+ *
+ * @category MT
+ * @package SmppBoxParser
+ */
 Class SmppBoxParser 
 {
-	public static function Parse($file) 
+	/**
+	 * State file for Apache2 logtail
+	 * 
+	 * @var string
+	 */
+	private static $_stateFile = './state';
+	
+	/**
+	 * Location of the Apache2 logtail2 binary
+	 * 
+	 * @var unknown_type
+	 */
+	private static $_logtailBin = '/usr/sbin/logtail2';
+
+	/**
+	 * Trigger the parsing process
+	 * 
+	 * @param string $logfile
+	 * @param string $stateFile
+	 * @param string $logtail
+	 * @return void
+	 */
+	public static function Parse($logfile, $stateFile = null, $logtail = null) 
 	{
 		$codes = array();
-		$cursor = self::GetCursor();
-
-		if (file_exists($file)) {
-			$handle = fopen($file, 'rb');
-			if ($handle) {
-                fseek($handle, $cursor, SEEK_SET);
-				while (!feof($handle)) {
-					$buffer = fgets($handle);
-					$matches = array();
-					preg_match_all('/^(\S+) (\S+) \[(\S+)\] \[(\S+)\] ERROR: \[.* (?P<code>\d{1,3}): .*\]: /', $buffer, $matches);
-					if (isset($matches['code'][0])) {
-						$codes[] = $matches['code'][0];
-					}
+		if (is_null($stateFile)) {
+			$stateFile = self::$_stateFile;
+		}
+		if (is_null($logtail)) {
+			$logtail = self::$_logtailBin;
+		}
+		if (file_exists($logfile)) {
+			$logOutput = shell_exec($logtail . " -f " . $logfile . " -o " . $stateFile);
+			$outputArray = explode("\n", $logOutput);
+			foreach ($outputArray as $line) {
+				preg_match_all('/^(\S+) (\S+) \[(\S+)\] \[(\S+)\] ERROR: \[.* (?P<code>\d{1,3}): .*\]: /', $line, $matches);
+				if (isset($matches['code'][0])) {
+					$codes[] = $matches['code'][0];
 				}
-                $line = ftell($handle);
-				fclose($handle);
 			}
 			self::Send($codes);
-			self::SaveCursor($line);
 		}
 	}
 	
+	/**
+	 * Loop parse results and call SendToCurl
+	 * 
+	 * @param array $codes
+	 * @return void
+	 */
 	protected static function Send($codes)
 	{
 		foreach ($codes as $code) {
@@ -33,33 +73,20 @@ Class SmppBoxParser
 		}
 	}
 	
+	/**
+	 * Send the stats to graphite
+	 * 
+	 * @param integer $code
+	 */
 	protected static function SendToCurl($code)
 	{
-		$fp = fsockopen("localhost", 2003, $errno, $errstr, 30);
+		$fp = fsockopen("localhost", 2023, $errno, $errstr, 30);
 		if (!$fp) {
 			echo "$errstr ($errno)<br />\n";
 		} else {
-            echo "carbon.installation.test_{$code} 1 " . time() . "\n";
-			fwrite($fp, "carbon.installation.test_{$code} 1 " . time() . "\n");
+			fwrite($fp, "server.dc1-sms-1.smppbox.errors.status 1");
+			fclose($fp);
 		}
-		fclose($fp);
-	}
-	
-	protected static function SaveCursor($num) 
-	{
-		$fp = fopen('./cursor', 'w+');
-		fwrite($fp, $num);
-		fclose($fp);
-	}
-	
-	protected static function GetCursor()
-	{
-        $file = './cursor';
-        if (!file_exists($file)) {
-            return 0;
-        }
-		$data = file_get_contents('./cursor');
-		return (strlen($data) == 0) ? 0 : $data;
 	}
 }
 
